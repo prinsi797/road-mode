034d564fa918411a91d3ff501edd96b59de0bf72
prinsi797 <prinsi@kryzetech.com>
2025-02-26 23:36:22 +0930

    seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,15 @@ class DatabaseSeeder extends Seeder {
         // $this->call(VehicalTableSeeder::class);
         // $this->call(ServiceCategorySeeder::class);
         // $this->call(CompanyMasterSeeder::class);
-        $this->call(ModelMasterSeeder::class);
+        // $this->call(ModelMasterSeeder::class);
 
-        // $this->call(UserTableSeeder::class);
-        // $this->call(RoleTableSeeder::class);
-        // $this->call(SettingTableSeeder::class);
-        // $this->call(PermissionsTableSeeder::class);
-        // $this->call(RoleHasPermissionsTableSeeder::class);
-        // $this->call(ModelHasRoleTableSeeder::class);
-        // $this->call(ModulesTableSeeder::class);
-        // $this->call(ModuleSchemasTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(SettingTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RoleHasPermissionsTableSeeder::class);
+        $this->call(ModelHasRoleTableSeeder::class);
+        $this->call(ModulesTableSeeder::class);
+        $this->call(ModuleSchemasTableSeeder::class);
     }
 }
